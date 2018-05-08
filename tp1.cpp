d4468e0dd8b8d195f729c011cc4998a4bb59d039
Alexis Aguirre <alex08lta@gmail.com>
2018-05-08 18:52:05 -0300

    eliminacion de comentarios refactorizxacion de codigo

--- tp1.cpp
+++ tp1.cpp
@@ -3,7 +3,6 @@
 #include <sstream>
 #include <cctype>
 
-
 //comentar que como es una lista de string, tengo que redefinir el nodo
 //resaltar que solo se cambia la parte int de la lista anterior por string
 using namespace std;
@@ -37,17 +36,17 @@ class Token{
 };
 
 class Nodo{
-    protected: 
-        Token *dato;
-        Nodo *next;
-    public:
-        Nodo(){next=NULL;};
-        Nodo(Token *a) {dato=a; next=NULL;};
-        void set_dato(Token *a) {dato=a; };
-        void set_next(Nodo *n) {next=n; };
-        Token *get_dato() {return dato; };
-        Nodo *get_next() {return next; };
-        bool es_vacio() {return next==NULL;}
+  protected: 
+    Token *dato;
+    Nodo *next;
+  public:
+    Nodo(){next=NULL;};
+    Nodo(Token *a) {dato=a; next=NULL;};
+    void set_dato(Token *a) {dato=a; };
+    void set_next(Nodo *n) {next=n; };
+    Token *get_dato() {return dato; };
+    Nodo *get_next() {return next; };
+    bool es_vacio() {return next==NULL;}
 };
 
 class Lista{
@@ -74,7 +73,6 @@ class Pila:public Lista{
 		void desapilar(void){del();};
 		bool pilavacia(){return esvacia();};
 };                  
-
 
 void Lista::del(void){ 
 	Nodo *aux;
@@ -117,163 +115,136 @@ string Lista::toPrint(string p){
 	}
 }
 
-///////////////// CALCULADORA ///////////////////////
-
 class Calculadora{
 	protected:
 		Pila *pilaOriginal;
-		/**
-		* Funcion encargada de procesar el entero recibido para operar con la pila
-		*
-		*/
-		void operar(Pila *p, int a){
-			int x;
-			if(p->pilavacia()){
-				x = a;
-			}else if(p->tope()->getType()=="number"){
-				x=p->tope()->getNumero()*10 + a;
-				p->desapilar(); //desapilo el numero y llamo a la funcion de nuevo con el nuevo numero
-				operar(p,x);
-			}else{
-				// si tiene un signo en el tope (lo que necesariamente no significa que haya otro numero antes que el signo)
-				// hay que ver si hay otro numero para hacer las operaciones, si no hay otro numero es porque es un signo menos
-				Token *tok = p->tope(); //guardo el signo que hay en la pila
-				p->desapilar(); //en este momento el p->tope() deberia tener un numero para operar
-				if(p->pilavacia()){
-				 if(tok->getChar() == '-')
-					x = -a;
-				}else{
-					switch( tok->getChar()){
-					case '+':
-//						p->desapilar();
-						x=p->tope()->getNumero() + a;
-						break;
-					case '-':
-//						p->desapilar();
-						x=p->tope()->getNumero() - a;
-						break;
-					case '*':
-//						p->desapilar();
-						x=p->tope()->getNumero() * a;
-						break;
-					case '/':
-//						p->desapilar();
-						x=p->tope()->getNumero() / a;
-						break;
-					}
-					p->desapilar(); //desapila el numero que estaba
-				}
-			}
-	    p->apilar(new Token(x));
-	    //cout << "\n\n ---- SALIENDO DE OPERAR ----- \n\n";
-		};
+		bool isnum(char valor);
+		bool analizar(string cad);
+		void operar(Pila *p, int a);
 		
 	public:
-		Calculadora(Pila *pila){
-			pilaOriginal = pila;
-		};
-		
-		int calcular(Pila *p){
-			Token val= *pilaOriginal->tope();
-			//----------VALORE TOKEN
-			/*
-			cout << "\nValor del Token: ";
-			if(val.getType()=="number"){
-				cout << pilaOriginal->tope()->getNumero();
-			}else{
-				cout << pilaOriginal->tope()->getChar();
-			}
-			//---------- EO VALORE TOKEN
-	  	cout << "\nPilaOriginal Actual:" << pilaOriginal->toPrint("");
-	  	cout << "\nPila P en trï¿½nsito: " << p->toPrint("");
-	  	*/
-			if(val.getType() == "number"){
-				operar(p,val.getNumero());
-				pilaOriginal->desapilar();
-			}else if(val.getChar() == '+' || val.getChar() == '-' || val.getChar() == '*' || val.getChar() == '/'){
-				pilaOriginal->desapilar();
-				p->apilar(&val);
-			}else if( val.getChar() == '('){
-				cout << "\nParentesis Abre";
-				pilaOriginal->desapilar(); //elimina el parentesis abierto
-				int valorSiguiente = calcular(new Pila());
-				cout << "\nValore siguiente: " << valorSiguiente;
-				
-				operar(p,valorSiguiente); // esto retorna un entero hay que hacer las operaciones necesarias
-			}else if(val.getChar() == ')'){
-				pilaOriginal->desapilar(); //elimina el parentesis cerrado
-				/*
-				int n = p->tope()->getNumero(); //guarda el valor tope de la pila actual
-				p->desapilar(); //desapila el numero
-				if(p->esvacia()){
-					cout << "\nRetornando " << n <<endl;
-					return n;
-				}else{
-					cout << "\nRetornando " << -n << endl;
-					return -n;
-				}*/
-				return p->tope()->getNumero();
-			}
-			if(! pilaOriginal->pilavacia() ){
-			  calcular(p);
-      }else{
-				return p->tope()->getNumero();
-      }
-      return p->tope()->getNumero();
-		}
-	bool isnum(char valor){
-	//comprobacion numero
+		Calculadora(Pila *pila){ pilaOriginal = pila; };
+		int calcular(Pila *p);
+};
+
+/**
+  * Determina si el caracter recibido es un número
+  * @param valor El caracter a analizar
+  * @return true si el valor es un número
+  */
+bool Calculadora::isnum(char valor){
 	bool isNum = false;
-		if(!isalpha(valor) && isalnum(valor))
-			isNum = true;
-		return isNum;
+	if(!isalpha(valor) && isalnum(valor))
+		isNum = true;
+	return isNum;
+}
+
+bool Calculadora::analizar(string cad){
+	// Analizar parentesis (cantidad y cerrado)
+	bool correcto= true;
+	Pila *parentesis = new Pila();
+	for(int i = 0 ; i < cad.length() ; i++){
+		if(cad.at(i)=='(')
+			parentesis->apilar(new Token('('));
+		else if(cad.at(i)==')')
+			if(parentesis->pilavacia())
+				correcto = false;
+			else
+				parentesis->desapilar();
 	}
-	bool analizar(string cad){
-		// Analizar parentesis (cantidad y cerrado)
-		bool correcto= true;
-		Pila *parentesis = new Pila();
-		for(int i = 0 ; i < cad.length() ; i++){
-			if(cad.at(i)=='(')
-				parentesis->apilar(new Token('('));
-			else if(cad.at(i)==')')
-				if(parentesis->pilavacia())
-					correcto = false;
-				else
-					parentesis->desapilar();
-		}
-		if(!parentesis->pilavacia())
-			correcto = false;
+	if(!parentesis->pilavacia())
+		correcto = false;
+	
 		
-			
-			
-		//analizador de imperfecciones
-		bool correcto1 = true;
-		string valor = "inicio";
-		if((!isnum(cad.at(0))
-			valor = "num";
-		else if(cad.at(0)=='(')
-				valor = "(";
-			else 
+		
+	//analizador de imperfecciones
+	bool correcto1 = true;
+	string valor = "inicio";
+	if((!isnum(cad.at(0))
+		valor = "num";
+	else if(cad.at(0)=='(')
+			valor = "(";
+		else 
+			correcto1 = false;
+	
+	for(int i = 1 ; i < cad.length() ; i++)
+	{
+		if(cad.at(i)=='(')
+			if(valor != "signo" && valor != "(")
 				correcto1 = false;
-		
-		for(int i = 1 ; i < cad.length() ; i++)
-		{
-			if(cad.at(i)=='(')
-				if(valor != "signo" && valor != "(")
+				else
+					valor = "(";
+		else if(cad.at(i)==')')
+				if(valor != "num" && valor != ")")
 					correcto1 = false;
 					else
-						valor = "(";
-			else if(cad.at(i)==')')
-					if(valor != "num" && valor != ")")
-						correcto1 = false;
-						else
-							valor = ")";
-				else if(cad.at(i)=='+' || cad.at(i)=='-' || cad.at(i)=='*' || cad.at(i)=='/')
+						valor = ")";
+			else if(cad.at(i)=='+' || cad.at(i)=='-' || cad.at(i)=='*' || cad.at(i)=='/')
+	}
+}
+
+int Calculadora::calcular(Pila *p){
+	Token val= *pilaOriginal->tope();
+	if(val.getType() == "number"){
+		operar(p,val.getNumero());
+		pilaOriginal->desapilar();
+	}else if(val.getChar() == '+' || val.getChar() == '-' || val.getChar() == '*' || val.getChar() == '/'){
+		pilaOriginal->desapilar();
+		p->apilar(&val);
+	}else if( val.getChar() == '('){
+		//cout << "\nParentesis Abre";
+		pilaOriginal->desapilar(); //elimina el parentesis abierto
+		int valorSiguiente = calcular(new Pila());
+		//cout << "\nValore siguiente: " << valorSiguiente;
+		operar(p,valorSiguiente); // esto retorna un entero hay que hacer las operaciones necesarias
+	}else if(val.getChar() == ')'){
+		pilaOriginal->desapilar(); //elimina el parentesis cerrado
+		return p->tope()->getNumero();
+	}
+	if(! pilaOriginal->pilavacia() ){
+	  calcular(p);
+  }else{
+		return p->tope()->getNumero();
+  }
+  return p->tope()->getNumero();
+}
+
+void Calculadora::calcular(Pila *p){
+	int x;
+	if(p->pilavacia()){
+		x = a;
+	}else if(p->tope()->getType()=="number"){
+		x=p->tope()->getNumero()*10 + a;
+		p->desapilar(); //desapilo el numero y llamo a la funcion de nuevo con el nuevo numero
+		operar(p,x);
+	}else{
+		// si tiene un signo en el tope (lo que necesariamente no significa que haya otro numero antes que el signo)
+		// hay que ver si hay otro numero para hacer las operaciones, si no hay otro numero es porque es un signo menos
+		Token *tok = p->tope(); //guardo el signo que hay en la pila
+		p->desapilar(); //en este momento el p->tope() deberia tener un numero para operar
+		if(p->pilavacia()){
+		 if(tok->getChar() == '-')
+			x = -a;
+		}else{
+			switch( tok->getChar()){
+			case '+':
+				x=p->tope()->getNumero() + a;
+				break;
+			case '-':
+				x=p->tope()->getNumero() - a;
+				break;
+			case '*':
+				x=p->tope()->getNumero() * a;
+				break;
+			case '/':
+				x=p->tope()->getNumero() / a;
+				break;
+			}
+			p->desapilar(); //desapila el numero que estaba
 		}
 	}
-		
+  p->apilar(new Token(x));
 };
-//////////////// FIN CALCULADORA //////////////////
 
 int main(){
   string cad;
